9f4765b1f0dd518b113aa4e0f4919a1082f354cb
Nicolás Palacio <nico.p_2011@live.com.ar>
2023-05-02 01:50:33 -0300

    Updated country json location

--- js/signUp.js
+++ js/signUp.js
@@ -20,7 +20,7 @@ form.addEventListener("submit",(e)=>{
 })
 
 const loadCountryList=async ()=>{
-    const response = await fetch("../countries.json");
+    const response = await fetch("countries.json");
     const countries = await response.json();
 
     let opt = document.createElement("option");
